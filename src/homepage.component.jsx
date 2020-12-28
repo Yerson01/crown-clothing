1aa2583ce4f1306b59c202e2b6f986cccf2eb093
yerson01 <yersonsoto84@gmail.com>
2020-12-28 19:39:56 -0400

    Seting up project with create-react-app

---
+++ src/homepage.component.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const Homepage = () => (
+    <div className="homepage">
+        <div className="menu-wrapper">
+            <div className="menu-item">
+                <div className="content">
+                    <h4 className="title">Title</h4>
+                    <p className="subtitle">Shop Now</p>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h4 className="title">Title</h4>
+                    <p className="subtitle">Shop Now</p>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h4 className="title">Title</h4>
+                    <p className="subtitle">Shop Now</p>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h4 className="title">Title</h4>
+                    <p className="subtitle">Shop Now</p>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h4 className="title">Title</h4>
+                    <p className="subtitle">Shop Now</p>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h4 className="title">Title</h4>
+                    <p className="subtitle">Shop Now</p>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default Homepage;
+
+
